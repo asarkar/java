79cbf76ff05dca430153483a623ec77571d67265
sarkara <sarkara@wclaccel-01du.oa.oclc.org>
2014-01-24 14:00:22 -0500

    Added a project to demonstrate JAXB XmlTransient annotation behavior

---
+++ xml/jaxb-xmltransient/src/main/java/name/abhijitsarkar/xml/jaxb/xmltransient/Car.java
@@ -0,0 +1,49 @@
+package name.abhijitsarkar.xml.jaxb.xmltransient;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlTransient
+public abstract class Car {
+	private String make;
+	private String model;
+	private int year;
+
+	public Car() {
+
+	}
+
+	public Car(String make, String model, int year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
