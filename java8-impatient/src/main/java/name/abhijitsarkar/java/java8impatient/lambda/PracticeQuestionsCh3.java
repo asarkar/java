2ac0793cd9912f4dc1d2d617d6282bee58272620
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2014-12-23 17:02:09 -0600

    Chapter 3 exercises

--- java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/lambda/PracticeQuestionsCh3.java
+++ java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/lambda/PracticeQuestionsCh3.java
@@ -15,10 +15,26 @@
  *******************************************************************************/
 package name.abhijitsarkar.java.java8impatient.lambda;
 
+import static java.lang.Character.isWhitespace;
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
+import static java.util.Comparator.naturalOrder;
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.of;
+
+import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
-import java.util.function.IntSupplier;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import org.slf4j.Logger;
@@ -97,7 +113,152 @@ public class PracticeQuestionsCh3 {
 
 	return isLoggingSuccessful;
     }
-    
-    public static void withLock(final Consumer<ReentrantLock> lock, final IntSupplier action) {
+
+    /**
+     * Q2: When you use a {@code ReentrantLock}, you are required to lock and
+     * unlock with the idiom
+     * 
+     * <pre>
+     * {@code
+     * myLock.lock();
+     * try {
+     *     some action
+     * } finally {
+     *     myLock.unlock();
+     * }
+     * </pre>
+     * 
+     * Provide a method withLock so that one can call {@code withLock(myLock,
+     * () -> some action })
+     * 
+     * @param lock
+     *            ReentrantLock.
+     * @param r
+     *            Action.
+     */
+    public static void withLock(final ReentrantLock lock, final Runnable r) {
+	lock.lock();
+
+	try {
+	    r.run();
+	} finally {
+	    lock.unlock();
+	}
+    }
+
+    public enum ComparatorCustomizationOption {
+	REVERSED, CASE_INSENSITIVE, SPACE_INSENSITIVE;
+    }
+
+    private static class SpaceInsensitiveComparator implements
+	    Comparator<String>, Serializable {
+	private static final long serialVersionUID = 43977305580825595L;
+
+	public int compare(final String s1, final String s2) {
+	    final int l1 = s1.length();
+	    final int l2 = s2.length();
+
+	    for (int i = 0, j = 0; i < l1 && j < l2;) {
+		final char c1 = s1.charAt(i);
+		final char c2 = s2.charAt(j);
+
+		if (isWhitespace(c1)) {
+		    i++;
+
+		    continue;
+		} else if (isWhitespace(c2)) {
+		    j++;
+
+		    continue;
+		}
+
+		if (c1 != c2) {
+		    return c1 - c2;
+		}
+	    }
+
+	    return 0;
+	}
+    }
+
+    /**
+     * Write a method that generates a {@code Comparator<String>} that can be
+     * normal or reversed, case-sensitive or case-insensitive, space-sensitive
+     * or space-insensitive, or any combination thereof. Your method should
+     * return a lambda expression.
+     * 
+     * @param customizationOptions
+     *            Comparator customization options.
+     * @return Comparator.
+     */
+    public static Comparator<String> comparatorGenerator(
+	    final ComparatorCustomizationOption... customizationOptions) {
+
+	final Map<ComparatorCustomizationOption, Comparator<String>> map = getComparatorCustomizationOptionsMap();
+
+	return of(customizationOptions).map(option -> map.get(option))
+		.reduce((c1, c2) -> c1.thenComparing(c2))
+		.orElse(naturalOrder());
+    }
+
+    static Map<ComparatorCustomizationOption, Comparator<String>> getComparatorCustomizationOptionsMap() {
+	final Map<ComparatorCustomizationOption, Comparator<String>> map = new HashMap<>();
+
+	map.put(ComparatorCustomizationOption.REVERSED, reverseOrder());
+	map.put(ComparatorCustomizationOption.CASE_INSENSITIVE,
+		CASE_INSENSITIVE_ORDER);
+	map.put(ComparatorCustomizationOption.SPACE_INSENSITIVE,
+		new SpaceInsensitiveComparator());
+
+	return map;
+    }
+
+    /**
+     * Q17: Implement a
+     * {@code doInParallelAsync(Runnable first, Runnable second, Consumer<Throwable> handler)}
+     * method that executes {@code first} and {@code second} in parallel,
+     * calling the handler if either method throws an exception.
+     * 
+     * @param first
+     *            First action.
+     * @param second
+     *            Second action.
+     * @param handler
+     *            Exception handler.
+     */
+    public static void doInParallelAsync(final Runnable first,
+	    final Runnable second, final Consumer<Throwable> handler) {
+	try {
+	    of(first, second).parallel().forEach(Runnable::run);
+	} catch (Throwable t) {
+	    handler.accept(t);
+	}
+    }
+
+    /**
+     * Q19: Look at the {@code Stream<T>} method
+     * {@code <U> U reduce (U identity, BiFunction<U, ? super T, U> accumulator, BinaryOperator<U> combiner)}
+     * . Should {@code U} be declared as {@code ? super U} in the first type
+     * argument to {@code BiFunction}? Why or why not?
+     * <p>
+     * <b>Ans:</b> Since {@code U} is the argument and the return type of
+     * {@code reduce}, the type does not vary. In effect, the contravariance and
+     * covariance cancel each other out.
+     */
+    public void existsOnlyToGlorifyJavadoc() {
+    }
+
+    /**
+     * Supply a static method
+     * {@code <T, U> List<U> map(List<T>, Function<T, U>)}.
+     * 
+     * @param l
+     *            input list.
+     * @param f
+     *            Function.
+     * @return Output list.
+     */
+    public static <T, U> List<U> map(List<T> l, Function<T, U> f) {
+	return l.stream().map(f).collect(toList());
     }
 }
--- java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/lambda/PracticeQuestionsCh3Test.java
+++ java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/lambda/PracticeQuestionsCh3Test.java
@@ -1,11 +1,29 @@
 package name.abhijitsarkar.java.java8impatient.lambda;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Arrays.asList;
+import static java.util.Arrays.sort;
 import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.capitalize;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.comparatorGenerator;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.doInParallelAsync;
 import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.isLoggingEnabledFor;
 import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.logIf;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.map;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.withLock;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.ComparatorCustomizationOption.CASE_INSENSITIVE;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.ComparatorCustomizationOption.REVERSED;
+import static name.abhijitsarkar.java.java8impatient.lambda.PracticeQuestionsCh3.ComparatorCustomizationOption.SPACE_INSENSITIVE;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -34,5 +52,117 @@ public class PracticeQuestionsCh3Test {
     public void testLogIfWhenFalse() {
 	assertFalse(logIf("error", () -> false,
 		() -> "This shouldn't be logged."));
+    }
+
+    @Test
+    public void testWithLock() throws IOException {
+	ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+	withLock(new ReentrantLock(), () -> {
+	    try {
+		os.write("1".getBytes(UTF_8));
+	    } catch (IOException e) {
+		throw new RuntimeException(e);
+	    }
+	});
+
+	assertEquals("1", os.toString(UTF_8.name()));
+
+	os.close();
+    }
+
+    @Test
+    public void testComparatorGeneratorReversed() {
+	String[] actual = new String[] { "a", "b", "c" };
+	String[] expected = new String[] { "c", "b", "a" };
+
+	sort(actual, comparatorGenerator(REVERSED));
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparatorGeneratorCaseInsensitive() {
+	String[] actual = new String[] { "a", "c", "B" };
+	String[] expected = new String[] { "a", "B", "c" };
+
+	sort(actual, comparatorGenerator(CASE_INSENSITIVE));
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparatorGeneratorCaseSensitive() {
+	String[] actual = new String[] { "a", "c", "B" };
+	String[] expected = new String[] { "B", "a", "c" };
+
+	sort(actual, comparatorGenerator());
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparatorGeneratorSpaceInsensitive() {
+	String[] actual = new String[] { "b ", "a", "c" };
+	String[] expected = new String[] { "a", "b ", "c" };
+
+	sort(actual, comparatorGenerator(SPACE_INSENSITIVE));
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparatorGeneratorSpaceSensitive() {
+	String[] actual = new String[] { "a", "c", " b" };
+	String[] expected = new String[] { " b", "a", "c" };
+
+	sort(actual, comparatorGenerator());
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testComparatorAllOptions() {
+	String[] actual = new String[] { "B", "a ", "c" };
+	String[] expected = new String[] { "c", "a ", "B" };
+
+	sort(actual,
+		comparatorGenerator(REVERSED, CASE_INSENSITIVE,
+			SPACE_INSENSITIVE));
+
+	assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testDoInParallelAsyncWhenNoException() {
+	Consumer<Throwable> handler = t -> t.printStackTrace();
+
+	doInParallelAsync(newRunnable(1, 1000), newRunnable(1001, 2000),
+		handler);
+    }
+
+    @Test
+    public void testDoInParallelAsyncWhenException() {
+	Consumer<Throwable> handler = t -> t.printStackTrace();
+
+	doInParallelAsync(newRunnable(1, 1000), () -> {
+	    throw new RuntimeException("Exception from second.");
+	}, handler);
+    }
+
+    private Runnable newRunnable(final int i, final int j) {
+	return () -> IntStream.range(i, j).forEach(System.out::println);
+    }
+
+    @Test
+    public void testMap() {
+	List<String> inputList = asList(new String[] { "1", "2" });
+
+	List<Integer> outputList = map(inputList, s -> Integer.parseInt(s));
+
+	assertEquals(2, outputList.size());
+
+	assertEquals(1, outputList.get(0).intValue());
+	assertEquals(2, outputList.get(1).intValue());
     }
 }
