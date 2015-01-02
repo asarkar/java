df64356a30e4991f6b895cdb74d159143f32ca7f
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-01-01 19:17:48 -0600

    Completed chapter 5 exercises.

--- java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/datentime/PracticeQuestionsCh5.java
+++ java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/datentime/PracticeQuestionsCh5.java
@@ -15,11 +15,13 @@
  *******************************************************************************/
 package name.abhijitsarkar.java.java8impatient.datentime;
 
+import static java.lang.Math.abs;
 import static java.lang.System.out;
 import static java.time.DayOfWeek.FRIDAY;
 import static java.time.DayOfWeek.MONDAY;
 import static java.time.DayOfWeek.SUNDAY;
 import static java.time.ZoneId.getAvailableZoneIds;
+import static java.time.temporal.ChronoUnit.MINUTES;
 import static java.time.temporal.TemporalAdjusters.firstDayOfNextMonth;
 import static java.util.stream.Collectors.toList;
 
@@ -29,6 +31,7 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 import java.util.List;
@@ -42,236 +45,255 @@ import org.slf4j.LoggerFactory;
  * @author Abhijit Sarkar
  */
 public class PracticeQuestionsCh5 {
-    public static final Logger LOGGER = LoggerFactory
-	    .getLogger(PracticeQuestionsCh5.class);
+	public static final Logger LOGGER = LoggerFactory.getLogger(PracticeQuestionsCh5.class);
 
-    /**
-     * Compute Porgrammer's Day without using {@code plusDays}.
-     * 
-     * @param year
-     *            The year for which to calculate Programmer's Day.
-     * @return A {@code LocalDate} instance representing the Programmer's Day
-     *         for the given year.
-     */
-    public static LocalDate getProgrammersDayForYear(final int year) {
-	return LocalDate.of(year, 1, 1).withDayOfYear(256);
-    }
-
-    /**
-     * Q2: What happens when you add one year to
-     * {@code LocalDate.of(2000, 2, 29)}? Four years? Four times one year?
-     * <p>
-     * <b>Ans:</b> Using method {@code plusYears}, adding 1 year too 2000-2-29
-     * yields 2001-02-28. Adding 4 years and 4 times 1 year yield 2004-02-29.
-     */
-    public void existsOnlyToGlorifyJavadoc() {
-    }
-
-    /**
-     * Q3: Implement a method {@code next} that takes a
-     * {@code Predicate<LocalDate>} and returns an adjuster yielding the next
-     * date fulfilling the predicate. For example,
-     * <code>today.with(next(w -> getDayOfWeek().getValue() < 6)</code> computes
-     * the next workday.
-     * <p>
-     * At first glance, it seems that the return type of the lambda expression
-     * is incompatible with the method return type. The lambda expr. returns a
-     * {@code Temporal} while the method returns a {@code TemporalAdjuster}.
-     * However, what actually returned here is a lambda expr. that accepts a
-     * {@code Temporal} and returns a {@code Temporal}. That's nothing but a
-     * {@code TemporalAdjuster}. Another point to be noted is that the compiler
-     * will supply the input {@code Temporal} based on the context of the call.
-     * 
-     * @param Predicate
-     *            . .
-     * @return TemporalAdjuster that fulfills the predicate.
-     */
-    public static TemporalAdjuster next(final Predicate<LocalDate> condition) {
-	return (localDate) -> {
-	    LocalDate input = LocalDate.from(localDate);
-	    for (; !condition.test(input); input = input.plusDays(1L))
-		;
-
-	    return input;
-	};
-    }
-
-    /**
-     * Q4: Write an equivalent of the Unix {@code cal} program that displays a
-     * calendar for a month. Show the weekend at the end of the week.
-     * <p>
-     * The output mirrors the Unix {@code cal} output, with 2 char day names and
-     * right justified single digit days.
-     * 
-     * @param year
-     *            The year.
-     * @param month
-     *            The month.
-     */
-    public static void cal(final int year, final int month) {
-	final String monthYear = Month.of(month).toString() + " " + year;
-
-	/*
-	 * The month year is output approximately in the center; the number of
-	 * leading spaces is calculated using the number of unused spaces on the
-	 * row and then halving it.
+	/**
+	 * Compute Porgrammer's Day without using {@code plusDays}.
+	 * 
+	 * @param year
+	 *            The year for which to calculate Programmer's Day.
+	 * @return A {@code LocalDate} instance representing the Programmer's Day for the given year.
 	 */
-	int width = (20 - monthYear.length()) / 2 + monthYear.length();
-
-	out.printf("\n%" + width + "s\n", monthYear);
-
-	for (DayOfWeek day : DayOfWeek.values()) {
-	    /*
-	     * Output 2 char day names. If Mon, use 2 spaces (means no leading
-	     * spaces), else use 3 (means 1 space between names).
-	     */
-	    out.printf("%" + (day.getValue() > 1 ? 3 : 2) + "s", day.toString()
-		    .substring(0, 2));
+	public static LocalDate getProgrammersDayForYear(final int year) {
+		return LocalDate.of(year, 1, 1).withDayOfYear(256);
 	}
-	out.printf("\n");
 
-	final LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
-	final LocalDate firstDayOfNextMonth = firstDayOfMonth
-		.with(firstDayOfNextMonth());
-
-	DayOfWeek dayOfWeek = null;
-	int dayOfMonth = 0;
-
-	for (LocalDate runningDate = firstDayOfMonth; runningDate
-		.isBefore(firstDayOfNextMonth); runningDate = runningDate
-		.plusDays(1)) {
-	    dayOfWeek = runningDate.getDayOfWeek();
-	    dayOfMonth = runningDate.getDayOfMonth();
-
-	    /*
-	     * If Mon, use 2 spaces for output (means no leading space when the
-	     * date is double digit and a leading space when the date is single
-	     * digit).
-	     */
-	    if (dayOfWeek == MONDAY) {
-		width = 2;
-	    }
-	    /*
-	     * If first day of the month, calculate the starting position; add
-	     * an additional space for right justification when the date is
-	     * single digit (< 10).
-	     */
-	    else if (runningDate == firstDayOfMonth) {
-		width = (dayOfWeek.getValue() - 1) * 3 + 1
-			+ (dayOfMonth < 10 ? 1 : 0);
-	    }
-	    /*
-	     * If not Mon or start of the month, use 3 spaces for output. This
-	     * will output single digit dates right justified.
-	     */
-	    else {
-		width = 3;
-	    }
-
-	    /* Output, breaking the line after Sun. */
-	    out.printf("%" + width + (dayOfWeek != SUNDAY ? "s" : "s\n"),
-		    dayOfMonth);
+	/**
+	 * Q2: What happens when you add one year to {@code LocalDate.of(2000, 2, 29)}? Four years? Four times one year?
+	 * <p>
+	 * <b>Ans:</b> Using method {@code plusYears}, adding 1 year too 2000-2-29 yields 2001-02-28. Adding 4 years and 4
+	 * times 1 year yield 2004-02-29.
+	 */
+	public void existsOnlyToGlorifyJavadoc() {
 	}
-    }
 
-    /**
-     * Q5: Write a program that prints how many days you have been alive.
-     * 
-     * @param year
-     *            Birth year.
-     * @param month
-     *            Birth month.
-     * @param dayOfMonth
-     *            Birth day of month.
-     * @return Age in days.
-     */
-    public static long ageInDays(final int year, final int month,
-	    final int dayOfMonth) {
-	final LocalDate dob = LocalDate.of(year, month, dayOfMonth);
-	final LocalDate now = LocalDate.now();
+	/**
+	 * Q3: Implement a method {@code next} that takes a {@code Predicate<LocalDate>} and returns an adjuster yielding
+	 * the next date fulfilling the predicate. For example,
+	 * <code>today.with(next(w -> getDayOfWeek().getValue() < 6)</code> computes the next workday.
+	 * <p>
+	 * At first glance, it seems that the return type of the lambda expression is incompatible with the method return
+	 * type. The lambda expr. returns a {@code Temporal} while the method returns a {@code TemporalAdjuster}. However,
+	 * what actually returned here is a lambda expr. that accepts a {@code Temporal} and returns a {@code Temporal}.
+	 * That's nothing but a {@code TemporalAdjuster}. Another point to be noted is that the compiler will supply the
+	 * input {@code Temporal} based on the context of the call.
+	 * 
+	 * @param Predicate
+	 *            . .
+	 * @return TemporalAdjuster that fulfills the predicate.
+	 */
+	public static TemporalAdjuster next(final Predicate<LocalDate> condition) {
+		return (localDate) -> {
+			LocalDate input = LocalDate.from(localDate);
+			for (; !condition.test(input); input = input.plusDays(1L))
+				;
 
-	return ChronoUnit.DAYS.between(dob, now);
-    }
+			return input;
+		};
+	}
 
-    /**
-     * List all Friday the 13th in the twentieth century.
-     * 
-     * @return List of all Friday the 13th in the twentieth century.
-     */
-    public static List<LocalDate> fridayTheThirteenth() {
-	final LocalDate startDate = LocalDate.of(1900, 1, 13);
+	/**
+	 * Q4: Write an equivalent of the Unix {@code cal} program that displays a calendar for a month. Show the weekend at
+	 * the end of the week.
+	 * <p>
+	 * The output mirrors the Unix {@code cal} output, with 2 char day names and right justified single digit days.
+	 * 
+	 * @param year
+	 *            The year.
+	 * @param month
+	 *            The month.
+	 */
+	public static void cal(final int year, final int month) {
+		final String monthYear = Month.of(month).toString() + " " + year;
 
-	return Stream.iterate(startDate, date -> date.plusMonths(1L))
-		.limit(100).filter(date -> date.getDayOfWeek() == FRIDAY)
-		.collect(toList());
+		/*
+		 * The month year is output approximately in the center; the number of leading spaces is calculated using the
+		 * number of unused spaces on the row and then halving it.
+		 */
+		int width = (20 - monthYear.length()) / 2 + monthYear.length();
 
-	/* Alternative implementation */
-	// final LocalDate startDate = LocalDate.of(1899, 12, 13);
-	// final LocalDate endDate = LocalDate.of(2000, 1, 13);
+		out.printf("\n%" + width + "s\n", monthYear);
 
-	// final UnaryOperator<LocalDate> nextFriDayTheThirteenth = (input) -> {
-	// LocalDate date = LocalDate.from(input).plusMonths(1);
-	//
-	// for (; date.getDayOfWeek() != FRIDAY; date = date.plusMonths(1))
-	// ;
-	//
-	// return date;
-	// };
+		for (DayOfWeek day : DayOfWeek.values()) {
+			/*
+			 * Output 2 char day names. If Mon, use 2 spaces (means no leading spaces), else use 3 (means 1 space
+			 * between names).
+			 */
+			out.printf("%" + (day.getValue() > 1 ? 3 : 2) + "s", day.toString().substring(0, 2));
+		}
+		out.printf("\n");
 
-	// final Stream.Builder<LocalDate> builder = Stream.builder();
-	//
-	// for (LocalDate runningDate = startDate
-	// .with(ofDateAdjuster(nextFriDayTheThirteenth)); runningDate
-	// .isBefore(endDate); runningDate = runningDate
-	// .with(ofDateAdjuster(nextFriDayTheThirteenth))) {
-	// builder.add(runningDate);
-	// }
-	//
-	// return builder.build().collect(toList());
-    }
+		final LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+		final LocalDate firstDayOfNextMonth = firstDayOfMonth.with(firstDayOfNextMonth());
 
-    /**
-     * Q8: Obtain the offsets of today's date in all supported time zones for
-     * the current time instant, turning {@code ZoneId.getAvailableIds} into a
-     * stream and using stream operations.
-     * 
-     * @return List of all offsets prefixed by zone ids.
-     */
-    public static List<String> getAllOffsets() {
-	final LocalDateTime now = LocalDateTime.now();
+		DayOfWeek dayOfWeek = null;
+		int dayOfMonth = 0;
 
-	return getAvailableZoneIds()
-		.stream()
-		.map(zoneId -> {
-		    final ZoneOffset offset = now.atZone(ZoneId.of(zoneId))
-			    .getOffset();
+		for (LocalDate runningDate = firstDayOfMonth; runningDate.isBefore(firstDayOfNextMonth); runningDate = runningDate
+				.plusDays(1)) {
+			dayOfWeek = runningDate.getDayOfWeek();
+			dayOfMonth = runningDate.getDayOfMonth();
 
-		    return zoneId + " " + offset.toString();
+			/*
+			 * If Mon, use 2 spaces for output (means no leading space when the date is double digit and a leading space
+			 * when the date is single digit).
+			 */
+			if (dayOfWeek == MONDAY) {
+				width = 2;
+			}
+			/*
+			 * If first day of the month, calculate the starting position; add an additional space for right
+			 * justification when the date is single digit (< 10).
+			 */
+			else if (runningDate == firstDayOfMonth) {
+				width = (dayOfWeek.getValue() - 1) * 3 + 1 + (dayOfMonth < 10 ? 1 : 0);
+			}
+			/*
+			 * If not Mon or start of the month, use 3 spaces for output. This will output single digit dates right
+			 * justified.
+			 */
+			else {
+				width = 3;
+			}
+
+			/* Output, breaking the line after Sun. */
+			out.printf("%" + width + (dayOfWeek != SUNDAY ? "s" : "s\n"), dayOfMonth);
+		}
+	}
+
+	/**
+	 * Q5: Write a program that prints how many days you have been alive.
+	 * 
+	 * @param year
+	 *            Birth year.
+	 * @param month
+	 *            Birth month.
+	 * @param dayOfMonth
+	 *            Birth day of month.
+	 * @return Age in days.
+	 */
+	public static long ageInDays(final int year, final int month, final int dayOfMonth) {
+		final LocalDate dob = LocalDate.of(year, month, dayOfMonth);
+		final LocalDate now = LocalDate.now();
+
+		return ChronoUnit.DAYS.between(dob, now);
+	}
+
+	/**
+	 * List all Friday the 13th in the twentieth century.
+	 * 
+	 * @return List of all Friday the 13th in the twentieth century.
+	 */
+	public static List<LocalDate> fridayTheThirteenth() {
+		final LocalDate startDate = LocalDate.of(1900, 1, 13);
+
+		return Stream.iterate(startDate, date -> date.plusMonths(1L)).limit(100)
+				.filter(date -> date.getDayOfWeek() == FRIDAY).collect(toList());
+
+		/* Alternative implementation */
+		// final LocalDate startDate = LocalDate.of(1899, 12, 13);
+		// final LocalDate endDate = LocalDate.of(2000, 1, 13);
+
+		// final UnaryOperator<LocalDate> nextFriDayTheThirteenth = (input) -> {
+		// LocalDate date = LocalDate.from(input).plusMonths(1);
+		//
+		// for (; date.getDayOfWeek() != FRIDAY; date = date.plusMonths(1))
+		// ;
+		//
+		// return date;
+		// };
+
+		// final Stream.Builder<LocalDate> builder = Stream.builder();
+		//
+		// for (LocalDate runningDate = startDate
+		// .with(ofDateAdjuster(nextFriDayTheThirteenth)); runningDate
+		// .isBefore(endDate); runningDate = runningDate
+		// .with(ofDateAdjuster(nextFriDayTheThirteenth))) {
+		// builder.add(runningDate);
+		// }
+		//
+		// return builder.build().collect(toList());
+	}
+
+	/**
+	 * Q8: Obtain the offsets of today's date in all supported time zones for the current time instant, turning
+	 * {@code ZoneId.getAvailableIds} into a stream and using stream operations.
+	 * 
+	 * @return List of all offsets prefixed by zone ids.
+	 */
+	public static List<String> getAllOffsets() {
+		final LocalDateTime now = LocalDateTime.now();
+
+		return getAvailableZoneIds().stream().map(zoneId -> {
+			final ZoneOffset offset = now.atZone(ZoneId.of(zoneId)).getOffset();
+
+			return zoneId + " " + offset.toString();
 		}).sorted().collect(toList());
-    }
+	}
 
-    /**
-     * Q9: Again using stream operations, find all time zones whose offsets
-     * aren't full hours.
-     * 
-     * @return List of all offsets that aren't full hours prefixed by zone ids.
-     */
-    public static List<String> getAllOffsetsWithFractionalHours() {
-	final LocalDateTime now = LocalDateTime.now();
+	/**
+	 * Q9: Again using stream operations, find all time zones whose offsets aren't full hours.
+	 * 
+	 * @return List of all offsets that aren't full hours prefixed by zone ids.
+	 */
+	public static List<String> getAllOffsetsWithFractionalHours() {
+		final LocalDateTime now = LocalDateTime.now();
 
-	return getAvailableZoneIds()
-		.stream()
-		.filter(zoneId -> {
-		    final ZoneOffset offset = now.atZone(ZoneId.of(zoneId))
-			    .getOffset();
+		return getAvailableZoneIds().stream().filter(zoneId -> {
+			final ZoneOffset offset = now.atZone(ZoneId.of(zoneId)).getOffset();
 
-		    return offset.getTotalSeconds() % 3600 != 0;
-		})
-		.map(zoneId -> {
-		    final ZoneOffset offset = now.atZone(ZoneId.of(zoneId))
-			    .getOffset();
+			return offset.getTotalSeconds() % 3600 != 0;
+		}).map(zoneId -> {
+			final ZoneOffset offset = now.atZone(ZoneId.of(zoneId)).getOffset();
 
-		    return zoneId + " " + offset.toString();
+			return zoneId + " " + offset.toString();
 		}).sorted().collect(toList());
-    }
+	}
+
+	/**
+	 * Your flight from Los Angeles to Frankfurt leaves at 3:05 pm local time and takes 10 hours and 50 minutes. When
+	 * does it arrive? Write a program that can handle calculations like this.
+	 * 
+	 * @param localZoneId
+	 *            Local zone id.
+	 * @param localDateTime
+	 *            Local datetime.
+	 * @param destinationZoneId
+	 *            Destination zone id.
+	 * @param flightDurationInMinutes
+	 *            Flight flight duration in minutes.
+	 * @return Destination datetime.
+	 */
+	public static LocalDateTime getLocalTimeAtDestination(final String localZoneId, final LocalDateTime localDateTime,
+			final String destinationZoneId, final long flightDurationInMinutes) {
+		final ZonedDateTime localZonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of(localZoneId));
+
+		return localZonedDateTime.plusMinutes(flightDurationInMinutes)
+				.withZoneSameInstant(ZoneId.of(destinationZoneId)).toLocalDateTime();
+	}
+
+	/**
+	 * Your return flight leaves Frankfurt at 14:05 and arrives in Los Angeles at 16:40. How long is the flight? Write a
+	 * program that can handle calculations like this.
+	 * 
+	 * @param localZoneId
+	 *            Local zone id.
+	 * @param localDateTime
+	 *            Local datetime.
+	 * @param destinationZoneId
+	 *            Destination zone id.
+	 * @param destinationlDateTime
+	 *            Destination datetime.
+	 * @return Flight duration in minutes.
+	 */
+	public static long getFlightDurationInMinutes(final String localZoneId, final LocalDateTime localDateTime,
+			final String destinationZoneId, final LocalDateTime destinationlDateTime) {
+		final ZoneId localZone = ZoneId.of(localZoneId);
+
+		final ZonedDateTime localZonedDateTime = ZonedDateTime.of(localDateTime, localZone);
+		final ZonedDateTime destinationZonedDateTime = ZonedDateTime.of(destinationlDateTime,
+				ZoneId.of(destinationZoneId));
+
+		return abs(MINUTES.between(destinationZonedDateTime.withZoneSameInstant(localZone), localZonedDateTime));
+	}
 }
--- java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/datentime/PracticeQuestionsCh5Test.java
+++ java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/datentime/PracticeQuestionsCh5Test.java
@@ -4,11 +4,14 @@ import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestions
 import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.fridayTheThirteenth;
 import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.getAllOffsets;
 import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.getAllOffsetsWithFractionalHours;
+import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.getFlightDurationInMinutes;
+import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.getLocalTimeAtDestination;
 import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.getProgrammersDayForYear;
 import static name.abhijitsarkar.java.java8impatient.datentime.PracticeQuestionsCh5.next;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.IntStream;
@@ -17,67 +20,87 @@ import org.junit.Test;
 
 public class PracticeQuestionsCh5Test {
 
-    @Test
-    public void testGetProgrammersDayForNonLeapYear() {
-	int year = 2014;
+	@Test
+	public void testGetProgrammersDayForNonLeapYear() {
+		int year = 2014;
 
-	LocalDate programmersDayExpected = LocalDate.of(year, 9, 13);
-	assertEquals(programmersDayExpected, getProgrammersDayForYear(year));
-    }
+		LocalDate programmersDayExpected = LocalDate.of(year, 9, 13);
+		assertEquals(programmersDayExpected, getProgrammersDayForYear(year));
+	}
 
-    @Test
-    public void testGetProgrammersDayForLeapYear() {
-	int year = 2016;
+	@Test
+	public void testGetProgrammersDayForLeapYear() {
+		int year = 2016;
 
-	LocalDate programmersDayExpected = LocalDate.of(year, 9, 12);
-	assertEquals(programmersDayExpected, getProgrammersDayForYear(year));
-    }
+		LocalDate programmersDayExpected = LocalDate.of(year, 9, 12);
+		assertEquals(programmersDayExpected, getProgrammersDayForYear(year));
+	}
 
-    @Test
-    public void testNextWithASaturday() {
-	LocalDate nextWorkingDay = LocalDate.of(2015, 1, 5);
+	@Test
+	public void testNextWithASaturday() {
+		LocalDate nextWorkingDay = LocalDate.of(2015, 1, 5);
 
-	assertEquals(
-		nextWorkingDay,
-		LocalDate.of(2015, 1, 3).with(
-			next(w -> w.getDayOfWeek().getValue() < 6)));
-    }
+		assertEquals(nextWorkingDay, LocalDate.of(2015, 1, 3).with(next(w -> w.getDayOfWeek().getValue() < 6)));
+	}
 
-    @Test
-    public void testNextWithAFriday() {
-	LocalDate nextWorkingDay = LocalDate.of(2015, 1, 2);
+	@Test
+	public void testNextWithAFriday() {
+		LocalDate nextWorkingDay = LocalDate.of(2015, 1, 2);
 
-	assertEquals(
-		nextWorkingDay,
-		LocalDate.of(2015, 1, 2).with(
-			next(w -> w.getDayOfWeek().getValue() < 6)));
-    }
+		assertEquals(nextWorkingDay, LocalDate.of(2015, 1, 2).with(next(w -> w.getDayOfWeek().getValue() < 6)));
+	}
 
-    @Test
-    public void testCal() {
-	/*
-	 * Not much can be tested for a void method that doesn't have side
-	 * effects; just print the calendars for the current year.
-	 */
-	Year currentYear = Year.now();
-	IntStream.rangeClosed(1, 12).forEach(
-		month -> cal(currentYear.getValue(), month));
-    }
+	@Test
+	public void testCal() {
+		/*
+		 * Not much can be tested for a void method that doesn't have side effects; just print the calendars for the
+		 * current year.
+		 */
+		Year currentYear = Year.now();
+		IntStream.rangeClosed(1, 12).forEach(month -> cal(currentYear.getValue(), month));
+	}
 
-    @Test
-    public void testFridayTheThirteenth() {
-	fridayTheThirteenth().forEach(
-		date -> System.out.println(DateTimeFormatter.ISO_DATE
-			.format(date)));
-    }
+	@Test
+	public void testFridayTheThirteenth() {
+		fridayTheThirteenth().forEach(date -> System.out.println(DateTimeFormatter.ISO_DATE.format(date)));
+	}
 
-    @Test
-    public void testGetAllOffsets() {
-	System.out.println(getAllOffsets());
-    }
+	@Test
+	public void testGetAllOffsets() {
+		System.out.println(getAllOffsets());
+	}
 
-    @Test
-    public void testGetAllOffsetsWithFractionalHours() {
-	System.out.println(getAllOffsetsWithFractionalHours());
-    }
+	@Test
+	public void testGetAllOffsetsWithFractionalHours() {
+		System.out.println(getAllOffsetsWithFractionalHours());
+	}
+
+	@Test
+	public void testGetLocalTimeAtDestination() {
+		String localZoneId = "America/Los_Angeles";
+		LocalDateTime localDateTime = LocalDateTime.of(2014, 12, 31, 15, 5);
+
+		/* Frankfurt is in the same time zone as Berlin and is 9 hours ahead of Los Angeles. */
+		String destinationZoneId = "Europe/Berlin";
+		long flightDurationInMinutes = 10 * 60 + 50;
+
+		LocalDateTime localTimeAtDestination = getLocalTimeAtDestination(localZoneId, localDateTime, destinationZoneId,
+				flightDurationInMinutes);
+
+		assertEquals(LocalDateTime.of(2015, 1, 1, 10, 55), localTimeAtDestination);
+	}
+
+	@Test
+	public void testGetFlightDurationInMinutes() {
+		String localZoneId = "America/Los_Angeles";
+		LocalDateTime localDateTime = LocalDateTime.of(2014, 12, 31, 15, 5);
+
+		/* Frankfurt is in the same time zone as Berlin and is 9 hours ahead of Los Angeles. */
+		String destinationZoneId = "Europe/Berlin";
+		LocalDateTime destinationDateTime = LocalDateTime.of(2015, 1, 1, 10, 55);
+
+		assertEquals(10 * 60 + 50,
+				getFlightDurationInMinutes(localZoneId, localDateTime, destinationZoneId, destinationDateTime));
+	}
+
 }
