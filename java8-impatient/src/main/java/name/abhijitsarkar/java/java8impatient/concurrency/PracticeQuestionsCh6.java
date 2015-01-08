7a2fe109c6539b5bada270a04f18406052e5f2cf
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-01-08 01:19:37 -0600

    More chapter 6 exercises.

--- java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/concurrency/PracticeQuestionsCh6.java
+++ java8-impatient/src/main/java/name/abhijitsarkar/java/java8impatient/concurrency/PracticeQuestionsCh6.java
@@ -20,13 +20,20 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.isDirectory;
 import static java.nio.file.Files.lines;
 import static java.nio.file.Files.walk;
+import static java.util.Arrays.parallelPrefix;
+import static java.util.Arrays.parallelSetAll;
+import static java.util.Collections.EMPTY_SET;
 import static java.util.Collections.unmodifiableMap;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static java.util.concurrent.ForkJoinPool.commonPool;
 import static java.util.function.BinaryOperator.maxBy;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static java.util.stream.Stream.of;
+import static org.jsoup.Jsoup.connect;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,8 +44,12 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.LongAdder;
@@ -50,203 +61,280 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PracticeQuestionsCh6 {
-    public static final Logger LOGGER = LoggerFactory
-	    .getLogger(PracticeQuestionsCh6.class);
+	public static final Logger LOGGER = LoggerFactory.getLogger(PracticeQuestionsCh6.class);
 
-    /**
-     * Q1: Write a program that keeps track of the longest string that is
-     * observed by a number of threads. Use an {@code AtomicReference} and an
-     * appropriate accumulator.
-     * 
-     * @param observed
-     *            Longest string.
-     * @param x
-     *            String value to be compared to the longest value.
-     * @return Longest string.
-     */
-    public static String updateLongestString(
-	    final AtomicReference<String> observed, final String x) {
+	/**
+	 * Q1: Write a program that keeps track of the longest string that is observed by a number of threads. Use an
+	 * {@code AtomicReference} and an appropriate accumulator.
+	 * 
+	 * @param observed
+	 *            Longest string.
+	 * @param x
+	 *            String value to be compared to the longest value.
+	 * @return Longest string.
+	 */
+	public static String updateLongestString(final AtomicReference<String> observed, final String x) {
 
-	final String longestString = observed.accumulateAndGet(x, maxBy((
-		currentValue, givenUpdate) -> {
-	    LOGGER.info("Received current value: {}, given update: {}",
-		    currentValue, givenUpdate);
+		final String longestString = observed.accumulateAndGet(x, maxBy((currentValue, givenUpdate) -> {
+			LOGGER.info("Received current value: {}, given update: {}", currentValue, givenUpdate);
 
-	    return currentValue.length() - givenUpdate.length();
-	}));
+			return currentValue.length() - givenUpdate.length();
+		}));
 
-	LOGGER.info("New observed: {}.", longestString);
+		LOGGER.info("New observed: {}.", longestString);
 
-	return longestString;
-    }
+		return longestString;
+	}
 
-    /**
-     * Q2: Does a {@code LongAdder} help with yielding a sequence of increasing
-     * IDs? Why or why not?
-     * <p>
-     * <b>Ans:</b> No, it's not helpful. From the Javadoc of method {@code sum},
-     * <i>"The returned value is NOT an atomic snapshot."</i>. It appears that
-     * {@code LongAdder} is designed so that multiple threads update a common
-     * sum that is used for purposes such as collecting statistics, but the sum
-     * is expected to be calculated outside of the multithreaded code.
-     */
-    public void existsOnlyToGlorifyJavadoc() {
-    }
+	/**
+	 * Q2: Does a {@code LongAdder} help with yielding a sequence of increasing IDs? Why or why not?
+	 * <p>
+	 * <b>Ans:</b> No, it's not helpful. From the Javadoc of method {@code sum},
+	 * <i>"The returned value is NOT an atomic snapshot."</i>. It appears that {@code LongAdder} is designed so that
+	 * multiple threads update a common sum that is used for purposes such as collecting statistics, but the sum is
+	 * expected to be calculated outside of the multithreaded code.
+	 */
+	public void existsOnlyToGlorifyJavadoc() {
+	}
 
-    /**
-     * Q3: Generate 1,000 threads, each of which increments a counter 100,000
-     * times. Compare the performance of using {@code AtomicLong} versus
-     * {@code LongAdder}.
-     */
-    public static void incrementUsingAtomicLong(final AtomicLong counter) {
-	run(() -> {
-	    counter.getAndIncrement();
-	});
-    }
+	/**
+	 * Q3: Generate 1,000 threads, each of which increments a counter 100,000 times. Compare the performance of using
+	 * {@code AtomicLong} versus {@code LongAdder}.
+	 */
+	public static void incrementUsingAtomicLong(final AtomicLong counter) {
+		run(() -> {
+			counter.getAndIncrement();
+		});
+	}
 
-    /**
-     * Q3: Generate 1,000 threads, each of which increments a counter 100,000
-     * times. Compare the performance of using {@code AtomicLong} versus
-     * {@code LongAdder}.
-     */
-    public static void incrementUsingLongAdder(final LongAdder counter) {
-	run(() -> {
-	    counter.increment();
-	});
-    }
+	/**
+	 * Q3: Generate 1,000 threads, each of which increments a counter 100,000 times. Compare the performance of using
+	 * {@code AtomicLong} versus {@code LongAdder}.
+	 */
+	public static void incrementUsingLongAdder(final LongAdder counter) {
+		run(() -> {
+			counter.increment();
+		});
+	}
 
-    @SuppressWarnings("unchecked")
-    private static void run(final Runnable task) {
-	final Instant start = Instant.now();
+	@SuppressWarnings("unchecked")
+	private static void run(final Runnable task) {
+		final Instant start = Instant.now();
 
-	LOGGER.info("Starting execution.");
+		LOGGER.info("Starting execution.");
 
-	new ForkJoinPool()
-		.invokeAll((Collection<? extends Callable<Void>>) IntStream
-			.range(0, 1000).mapToObj(i -> {
-			    return new Callable<Void>() {
+		new ForkJoinPool().invokeAll((Collection<? extends Callable<Void>>) IntStream.range(0, 1000).mapToObj(i -> {
+			return new Callable<Void>() {
 				@Override
 				public Void call() {
-				    for (int j = 0; j < 100000; j++) {
-					try {
-					    task.run();
-					} catch (Exception e) {
-					    LOGGER.error(
-						    "Something went wrong. Get the hell outta here.",
-						    e);
+					for (int j = 0; j < 100000; j++) {
+						try {
+							task.run();
+						} catch (Exception e) {
+							LOGGER.error("Something went wrong. Get the hell outta here.", e);
 
-					    break;
+							break;
+						}
 					}
-				    }
 
-				    return null;
+					return null;
 				}
-			    };
-			}).collect(toList()));
+			};
+		}).collect(toList()));
 
-	LOGGER.info("Finished execution.");
+		LOGGER.info("Finished execution.");
 
-	final Instant end = Instant.now();
+		final Instant end = Instant.now();
 
-	final Duration runningTime = Duration.between(start, end);
+		final Duration runningTime = Duration.between(start, end);
 
-	LOGGER.info("Running time: {} seconds {} nanoseconds.",
-		runningTime.getSeconds(), runningTime.getNano());
-    }
-
-    /**
-     * Q5: Write an application in which multiple threads read all words from a
-     * collection of files. Use a {@code ConcurrentHashMap<String, Set<File>>}
-     * to track in which files each word occurs. Use the {@code merge} method to
-     * update the map.
-     * 
-     * @param path
-     *            Input File or root directory.
-     * @return Map containing entries for each word vs the files in which it
-     *         occurs.
-     * @throws IOException
-     */
-    public static Map<String, Set<File>> reverseIndexUsingMerge(final Path path)
-	    throws IOException {
-	final ForkJoinPool pool = new ForkJoinPool();
-
-	final ConcurrentHashMap<String, Set<File>> map = new ConcurrentHashMap<>();
-
-	final BiConsumer<? super String, ? super Set<File>> action = (key,
-		value) -> map.merge(key, value, (existingValue, newValue) -> {
-	    LOGGER.info("Received key: {}, existing value: {}, new value: {}.",
-		    key, existingValue, newValue);
-
-	    newValue.addAll(existingValue);
-
-	    return newValue;
-	});
-
-	pool.invokeAll(walk(path, 1).map(p -> new ReverseIndex(p, action))
-		.collect(toList()));
-
-	return unmodifiableMap(map);
-    }
-
-    static class ReverseIndex implements Callable<Void> {
-	private final Path p;
-	private final BiConsumer<? super String, ? super Set<File>> action;
-
-	private ReverseIndex(final Path p,
-		final BiConsumer<? super String, ? super Set<File>> action) {
-	    this.p = p;
-	    this.action = action;
+		LOGGER.info("Running time: {} seconds {} nanoseconds.", runningTime.getSeconds(), runningTime.getNano());
 	}
 
-	@Override
-	public Void call() throws Exception {
-	    try {
-		LOGGER.info("Visited {}.", p);
+	/**
+	 * Q5: Write an application in which multiple threads read all words from a collection of files. Use a
+	 * {@code ConcurrentHashMap<String, Set<File>>} to track in which files each word occurs. Use the {@code merge}
+	 * method to update the map.
+	 * 
+	 * @param path
+	 *            Input File or root directory.
+	 * @return Map containing entries for each word vs the files in which it occurs.
+	 * @throws IOException
+	 */
+	public static Map<String, Set<File>> reverseIndexUsingMerge(final Path path) throws IOException {
+		final ForkJoinPool pool = new ForkJoinPool();
 
-		if (isDirectory(p)) {
-		    return null;
+		final ConcurrentHashMap<String, Set<File>> map = new ConcurrentHashMap<>();
+
+		final BiConsumer<? super String, ? super Set<File>> action = (key, value) -> map.merge(key, value, (
+				existingValue, newValue) -> {
+			LOGGER.info("Received key: {}, existing value: {}, new value: {}.", key, existingValue, newValue);
+
+			newValue.addAll(existingValue);
+
+			return newValue;
+		});
+
+		pool.invokeAll(walk(path, 1).map(p -> new ReverseIndex(p, action)).collect(toList()));
+
+		return unmodifiableMap(map);
+	}
+
+	private static class ReverseIndex implements Callable<Void> {
+		private final Path p;
+		private final BiConsumer<? super String, ? super Set<File>> action;
+
+		private ReverseIndex(final Path p, final BiConsumer<? super String, ? super Set<File>> action) {
+			this.p = p;
+			this.action = action;
 		}
 
-		reverseIndex().forEach(action);
-	    } catch (IOException ie) {
-		LOGGER.error("Something went wrong. Get the hell outta here.",
-			ie);
-	    }
+		@Override
+		public Void call() throws Exception {
+			try {
+				LOGGER.info("Visited {}.", p);
 
-	    return null;
+				if (isDirectory(p)) {
+					return null;
+				}
+
+				reverseIndex().forEach(action);
+			} catch (IOException ie) {
+				LOGGER.error("Something went wrong. Get the hell outta here.", ie);
+			}
+
+			return null;
+		}
+
+		final Consumer<? super String> debugAction = word -> {
+			final int count = p.getNameCount();
+
+			LOGGER.info("File: {}, word: {}", p.getName(count > 0 ? count - 1 : 0), word);
+		};
+
+		private Map<String, Set<File>> reverseIndex() throws IOException {
+			return lines(p, UTF_8).flatMap(line -> of(line.split("\\s")))
+			// .peek(debugAction)
+					.collect(groupingBy(String::toString, mapping(word -> p.toFile(), toSet())));
+		}
 	}
 
-	final Consumer<? super String> debugAction = word -> {
-	    final int count = p.getNameCount();
+	/**
+	 * Q7: In a {@code ConcurrentHashMap<String, Long>}, find the key with the maximum value (breaking ties
+	 * arbitrarily).
+	 * 
+	 * @param map
+	 *            Map.
+	 * @return Key that has the maximum value.
+	 */
+	public static String getKeyWithMaxValue(final ConcurrentHashMap<String, Long> map) {
+		final int parallelismThreshold = getRuntime().availableProcessors();
 
-	    LOGGER.info("File: {}, word: {}",
-		    p.getName(count > 0 ? count - 1 : 0), word);
-	};
-
-	private Map<String, Set<File>> reverseIndex() throws IOException {
-	    return lines(p, UTF_8).flatMap(line -> of(line.split("\\s")))
-	    // .peek(debugAction)
-		    .collect(
-			    groupingBy(String::toString,
-				    mapping(word -> p.toFile(), toSet())));
+		return map
+				.reduceEntries(parallelismThreshold, (e1, e2) -> e1.getValue().compareTo(e2.getValue()) > 0 ? e1 : e2)
+				.getKey();
 	}
-    }
 
-    /**
-     * Q7: In a {@code ConcurrentHashMap<String, Long>}, find the key with the
-     * maximum value (breaking ties arbitrarily).
-     * 
-     * @param map
-     *            Map.
-     * @return Key that has the maximum value.
-     */
-    public static String getKeyWithMaxValue(
-	    final ConcurrentHashMap<String, Long> map) {
-	final int parallelismThreshold = getRuntime().availableProcessors();
+	/**
+	 * You can use the {@code parallelPrefix} method to parallelize the computation of Fibonacci numbers. We use the
+	 * fact that the nth Fibonacci number is the top left coefficient of F<sup>n</sup>, where F = (1, 1, 1, 0). Make an
+	 * array filled with 2 x 2 matrices. Define a {@code Matrix} class with a multiplication method, use
+	 * {@code parallelSetAll} to make an array of matrices, and use {@code parallelPefix} to multiply them.
+	 * <p>
+	 * Fibonacci series: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+	 * 
+	 * @param n
+	 *            The index of the Fibonacci number to return.
+	 * @return The nth Fibonacci number.
+	 */
+	public static int fibonacci(final int n) {
+		if (n <= 2) {
+			return 1;
+		}
 
-	return map.reduceEntries(
-		parallelismThreshold,
-		(e1, e2) -> e1.getValue().compareTo(e2.getValue()) > 0 ? e1
-			: e2).getKey();
-    }
+		final TwoByTwoMatrix[] matrices = new TwoByTwoMatrix[n - 1];
+
+		parallelSetAll(matrices, i -> new TwoByTwoMatrix());
+
+		LOGGER.info("Matrix array after parallelSetAll.\n");
+		printMatrices(matrices);
+
+		parallelPrefix(matrices, TwoByTwoMatrix::multiply);
+
+		LOGGER.info("Matrix array after parallelPrefix.\n");
+		printMatrices(matrices);
+
+		/* The last matrix in the array is the product of multiplying all elements in the array. */
+		return matrices[n - 2].topLeftCoefficient;
+	}
+
+	private static void printMatrices(final TwoByTwoMatrix[] matrices) {
+		for (int i = 0; i < matrices.length; i++) {
+			LOGGER.info("[{}] -> {}.", i, matrices[i]);
+		}
+	}
+
+	private static class TwoByTwoMatrix {
+		private int topLeftCoefficient;
+		private int topRightCoefficient;
+		private int bottomLeftCoefficient;
+		private int bottomRightCoefficient;
+
+		TwoByTwoMatrix() {
+			this(1, 1, 1, 0);
+		}
+
+		TwoByTwoMatrix(final int topLeftCoefficient, final int topRightCoefficient, final int bottomLeftCoefficient,
+				final int bottomRightCoefficient) {
+			this.topLeftCoefficient = topLeftCoefficient;
+			this.topRightCoefficient = topRightCoefficient;
+			this.bottomLeftCoefficient = bottomLeftCoefficient;
+			this.bottomRightCoefficient = bottomRightCoefficient;
+		}
+
+		/* https://www.youtube.com/watch?v=xMNOI8YRQIo */
+		TwoByTwoMatrix multiply(final TwoByTwoMatrix other) {
+			final int productTopLeftCoefficient = topLeftCoefficient * other.topLeftCoefficient + topRightCoefficient
+					* other.bottomLeftCoefficient;
+			final int productTopRightCoefficient = topLeftCoefficient * other.topRightCoefficient + topRightCoefficient
+					* other.bottomRightCoefficient;
+			final int productBottomLeftCoefficient = bottomLeftCoefficient * other.topLeftCoefficient
+					+ bottomRightCoefficient * other.bottomLeftCoefficient;
+			final int productBottomRightCoefficient = bottomLeftCoefficient * other.topRightCoefficient
+					+ bottomRightCoefficient * other.bottomRightCoefficient;
+
+			return new TwoByTwoMatrix(productTopLeftCoefficient, productTopRightCoefficient,
+					productBottomLeftCoefficient, productBottomRightCoefficient);
+		}
+
+		@Override
+		public String toString() {
+			return "TwoByTwoMatrix [[0,0]=" + topLeftCoefficient + ", [0,1]=" + topRightCoefficient + ", [1,0]="
+					+ bottomLeftCoefficient + ", [1,1]=" + bottomRightCoefficient + "]";
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Set<String> getURLs(final String url) throws InterruptedException, ExecutionException {
+		final ConcurrentLinkedQueue<String> urls = new ConcurrentLinkedQueue<>();
+
+		supplyAsync(
+				() -> {
+					try {
+						return connect(url).get();
+					} catch (Exception e) {
+						throw new CompletionException(
+								"Cannot complete request: Something went wrong trying to fetch the contents of the URL: "
+										+ url + ".", e);
+					}
+				})
+				.thenApply(doc -> doc.select("a[href]"))
+				.thenAccept(
+						links -> urls.addAll((Collection<? extends String>) links.stream().map(
+								link -> link.attr("abs:href")))).get();
+
+		final boolean isComplete = commonPool().awaitQuiescence(10, TimeUnit.SECONDS);
+
+		return isComplete ? unmodifiableSet(urls.stream().collect(toSet())) : EMPTY_SET;
+	}
 }
--- java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/concurrency/PracticeQuestionsCh6Test.java
+++ java8-impatient/src/test/java/name/abhijitsarkar/java/java8impatient/concurrency/PracticeQuestionsCh6Test.java
@@ -21,7 +21,9 @@ import static java.lang.System.gc;
 import static java.lang.Thread.currentThread;
 import static java.nio.file.Paths.get;
 import static java.util.stream.Collectors.joining;
+import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.fibonacci;
 import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.getKeyWithMaxValue;
+import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.getURLs;
 import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.incrementUsingAtomicLong;
 import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.incrementUsingLongAdder;
 import static name.abhijitsarkar.java.java8impatient.concurrency.PracticeQuestionsCh6.reverseIndexUsingMerge;
@@ -36,6 +38,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
@@ -55,153 +58,156 @@ import org.slf4j.LoggerFactory;
  * @author Abhijit Sarkar
  */
 public class PracticeQuestionsCh6Test {
-    public static final Logger LOGGER = LoggerFactory
-	    .getLogger(PracticeQuestionsCh6Test.class);
+	public static final Logger LOGGER = LoggerFactory.getLogger(PracticeQuestionsCh6Test.class);
 
-    @Test
-    public void testUpdateLongestString() {
-	final String[] words = new String[] { "Java 8", "Java 8 is Awesome!",
-		"Java 8 is the Best thing Since Sliced Bread!",
-		"Java 8 Changes Everything!" };
-	final int len = words.length;
-	final int stopAfter = 100;
+	@Test
+	public void testUpdateLongestString() {
+		final String[] words = new String[] { "Java 8", "Java 8 is Awesome!",
+				"Java 8 is the Best thing Since Sliced Bread!", "Java 8 Changes Everything!" };
+		final int len = words.length;
+		final int stopAfter = 100;
 
-	final AtomicReference<String> longestString = new AtomicReference<>(
-		words[0]);
-	final AtomicInteger count = new AtomicInteger(1);
+		final AtomicReference<String> longestString = new AtomicReference<>(words[0]);
+		final AtomicInteger count = new AtomicInteger(1);
 
-	class UpdateLongestStringTask extends RecursiveAction {
-	    private static final long serialVersionUID = -2288401002001447054L;
+		class UpdateLongestStringTask extends RecursiveAction {
+			private static final long serialVersionUID = -2288401002001447054L;
 
-	    private int id = -1;
+			private int id = -1;
 
-	    private UpdateLongestStringTask(final int id) {
-		this.id = id;
-	    }
+			private UpdateLongestStringTask(final int id) {
+				this.id = id;
+			}
 
-	    @Override
-	    protected void compute() {
-		LOGGER.info("Executing task #: {}.", id);
+			@Override
+			protected void compute() {
+				LOGGER.info("Executing task #: {}.", id);
 
-		if (count.get() >= stopAfter) {
-		    return;
+				if (count.get() >= stopAfter) {
+					return;
+				}
+
+				final ForkJoinTask<Void> task = new UpdateLongestStringTask(count.incrementAndGet()).fork();
+
+				updateLongestString(longestString, words[randomIndex()]);
+
+				task.join();
+			}
+
+			private int randomIndex() {
+				/*
+				 * From the Javadoc: "Instances of java.util.Random are threadsafe. However, the concurrent use of the
+				 * same java.util.Random instance across threads may encounter contention and consequent poor
+				 * performance. Consider instead using ThreadLocalRandom in multithreaded designs."
+				 */
+				return ThreadLocalRandom.current().nextInt(len);
+			}
 		}
 
-		final ForkJoinTask<Void> task = new UpdateLongestStringTask(
-			count.incrementAndGet()).fork();
+		/* Just because we can. */
+		final int parallelism = min(getRuntime().availableProcessors(), 4);
 
-		updateLongestString(longestString, words[randomIndex()]);
-
-		task.join();
-	    }
-
-	    private int randomIndex() {
-		/*
-		 * From the Javadoc: "Instances of java.util.Random are
-		 * threadsafe. However, the concurrent use of the same
-		 * java.util.Random instance across threads may encounter
-		 * contention and consequent poor performance. Consider instead
-		 * using ThreadLocalRandom in multithreaded designs."
-		 */
-		return ThreadLocalRandom.current().nextInt(len);
-	    }
+		new ForkJoinPool(parallelism).invoke(new UpdateLongestStringTask(count.get()));
 	}
 
-	/* Just because we can. */
-	final int parallelism = min(getRuntime().availableProcessors(), 4);
+	@Ignore("Takes too long")
+	public void testIncrementUsingAtomicLong() {
+		LOGGER.info("Warming up\n.");
 
-	new ForkJoinPool(parallelism).invoke(new UpdateLongestStringTask(count
-		.get()));
-    }
+		/* Warm up */
+		for (int i = 0; i < 5; i++) {
+			incrementUsingAtomicLong(new AtomicLong(0));
+		}
 
-    @Ignore("Takes too long")
-    public void testIncrementUsingAtomicLong() {
-	LOGGER.info("Warming up\n.");
+		LOGGER.info("Rest and cleanup\n.");
 
-	/* Warm up */
-	for (int i = 0; i < 5; i++) {
-	    incrementUsingAtomicLong(new AtomicLong(0));
+		restAndCleanup();
+
+		LOGGER.info("Do it now!\n");
+
+		AtomicLong counter = new AtomicLong(0);
+
+		incrementUsingAtomicLong(counter);
+
+		assertEquals(100000000, counter.get());
 	}
 
-	LOGGER.info("Rest and cleanup\n.");
+	@Ignore("Takes too long")
+	public void testIncrementUsingLongAdder() {
+		LOGGER.info("Warming up\n.");
 
-	restAndCleanup();
+		/* Warm up */
+		for (int i = 0; i < 5; i++) {
+			incrementUsingLongAdder(new LongAdder());
+		}
 
-	LOGGER.info("Do it now!\n");
+		LOGGER.info("Rest and cleanup\n.");
 
-	AtomicLong counter = new AtomicLong(0);
+		restAndCleanup();
 
-	incrementUsingAtomicLong(counter);
+		LOGGER.info("Do it now!\n");
 
-	assertEquals(100000000, counter.get());
-    }
+		LongAdder counter = new LongAdder();
 
-    @Ignore("Takes too long")
-    public void testIncrementUsingLongAdder() {
-	LOGGER.info("Warming up\n.");
+		incrementUsingLongAdder(counter);
 
-	/* Warm up */
-	for (int i = 0; i < 5; i++) {
-	    incrementUsingLongAdder(new LongAdder());
+		assertEquals(100000000, counter.longValue());
 	}
 
-	LOGGER.info("Rest and cleanup\n.");
+	@SuppressWarnings("static-access")
+	private void restAndCleanup() {
+		gc();
 
-	restAndCleanup();
-
-	LOGGER.info("Do it now!\n");
-
-	LongAdder counter = new LongAdder();
-
-	incrementUsingLongAdder(counter);
-
-	assertEquals(100000000, counter.longValue());
-    }
-
-    @SuppressWarnings("static-access")
-    private void restAndCleanup() {
-	gc();
-
-	try {
-	    currentThread().sleep(2000);
-	} catch (InterruptedException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    @Test
-    public void testReverseIndexUsingMerge() throws IOException,
-	    URISyntaxException {
-	Path p = get(getClass().getResource("/ch6").toURI());
-
-	Map<String, Set<File>> reverseIndex = reverseIndexUsingMerge(p);
-
-	String filesContainingTheWordJava = joinValues("Java", reverseIndex);
-
-	assertEquals("f1.txt,f2.txt,f3.txt", filesContainingTheWordJava);
-
-	String filesContainingTheWordIs = joinValues("is", reverseIndex);
-
-	assertEquals("f1.txt,f2.txt", filesContainingTheWordIs);
-    }
-
-    private String joinValues(String key, Map<String, Set<File>> map) {
-	if (!map.containsKey(key)) {
-	    throw new NoSuchElementException("No entry exists for key: " + key);
+		try {
+			currentThread().sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
-	return map.get(key).stream().map(File::getName).sorted()
-		.collect(joining(","));
-    }
+	@Test
+	public void testReverseIndexUsingMerge() throws IOException, URISyntaxException {
+		Path p = get(getClass().getResource("/ch6").toURI());
 
-    @Test
-    public void testGetKeyWithMaxValue() {
-	ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+		Map<String, Set<File>> reverseIndex = reverseIndexUsingMerge(p);
 
-	LongStream.range(1, 10).forEach(l -> map.put(String.valueOf(l), l));
+		String filesContainingTheWordJava = joinValues("Java", reverseIndex);
 
-	String key = getKeyWithMaxValue(map);
+		assertEquals("f1.txt,f2.txt,f3.txt", filesContainingTheWordJava);
 
-	assertEquals("9", key);
-    }
+		String filesContainingTheWordIs = joinValues("is", reverseIndex);
+
+		assertEquals("f1.txt,f2.txt", filesContainingTheWordIs);
+	}
+
+	private String joinValues(String key, Map<String, Set<File>> map) {
+		if (!map.containsKey(key)) {
+			throw new NoSuchElementException("No entry exists for key: " + key);
+		}
+
+		return map.get(key).stream().map(File::getName).sorted().collect(joining(","));
+	}
+
+	@Test
+	public void testGetKeyWithMaxValue() {
+		ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+
+		LongStream.range(1, 10).forEach(l -> map.put(String.valueOf(l), l));
+
+		String key = getKeyWithMaxValue(map);
+
+		assertEquals("9", key);
+	}
+
+	@Test
+	public void testFibonacci() {
+		assertEquals(1, fibonacci(-1));
+		assertEquals(1, fibonacci(2));
+		assertEquals(8, fibonacci(6));
+	}
+
+	@Test
+	public void testGetURLs() throws InterruptedException, ExecutionException {
+		getURLs("http://en.wikipedia.org/wiki/Java_%28programming_language%29").forEach(System.out::println);
+	}
 }
