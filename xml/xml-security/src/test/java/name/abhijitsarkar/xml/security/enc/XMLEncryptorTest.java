8ba89906806bb766ec7a4d38c5858830679f9593
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-02-05 10:36:41 -0600

    Formatting changes.

--- xml/xml-security/src/test/java/name/abhijitsarkar/xml/security/enc/XMLEncryptorTest.java
+++ xml/xml-security/src/test/java/name/abhijitsarkar/xml/security/enc/XMLEncryptorTest.java
@@ -2,9 +2,6 @@ package name.abhijitsarkar.xml.security.enc;
 
 import java.io.File;
 
-import name.abhijitsarkar.xml.security.enc.XMLEncryptor;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 public class XMLEncryptorTest {
@@ -13,32 +10,22 @@ public class XMLEncryptorTest {
     private static final String OUTPUT_FILE = "EncryptedPurchaseOrder.xml";
 
     @Test
-    public void testEncrypt() {
+    public void testEncrypt() throws Exception {
 	String inputFile = XMLEncryptorTest.class.getResource("/" + INPUT_FILE)
 		.getPath();
 	String outputFile = new File(inputFile).getParent() + File.separator
 		+ OUTPUT_FILE;
 
-	try {
-	    XMLEncryptor.encrypt(inputFile, outputFile);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    Assert.fail("Shouldn't be here.");
-	}
+	XMLEncryptor.encrypt(inputFile, outputFile);
     }
 
     @Test
-    public void testDecrypt() {
+    public void testDecrypt() throws Exception {
 	String inputFile = XMLEncryptorTest.class.getResource("/" + INPUT_FILE)
 		.getPath();
 	String outputFile = new File(inputFile).getParent() + File.separator
 		+ OUTPUT_FILE;
 
-	try {
-	    XMLEncryptor.decrypt(outputFile);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    Assert.fail("Shouldn't be here.");
-	}
+	XMLEncryptor.decrypt(outputFile);
     }
 }
